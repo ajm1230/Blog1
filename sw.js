cf620ff0c285116b5e6bc9755f55ba4258864116
ajm1230 <155264455+ajm1230@users.noreply.github.com>
2025-05-20 13:21:41 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+<!-- PushAlert -->
+<script type="text/javascript">
+        (function(d, t) {
+                var g = d.createElement(t),
+                s = d.getElementsByTagName(t)[0];
+                g.src = "https://cdn.pushalert.co/integrate_0bdfae75b5ddbff7a5e53338a7cf00ed.js";
+                s.parentNode.insertBefore(g, s);
+        }(document, "script"));
+</script>
+<!-- End PushAlert -->
