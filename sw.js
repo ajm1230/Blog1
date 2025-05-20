f0dc6857661e1f4a9812dc2e467f5a2e8c4a3031
ajm1230 <155264455+ajm1230@users.noreply.github.com>
2025-05-20 13:28:57 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-<!-- PushAlert -->
-<script type="text/javascript">
-        (function(d, t) {
-                var g = d.createElement(t),
-                s = d.getElementsByTagName(t)[0];
-                g.src = "https://cdn.pushalert.co/integrate_0bdfae75b5ddbff7a5e53338a7cf00ed.js";
-                s.parentNode.insertBefore(g, s);
-        }(document, "script"));
-</script>
-<!-- End PushAlert -->
+importScripts("https://cdn.pushalert.co/sw-69966_2.js");
